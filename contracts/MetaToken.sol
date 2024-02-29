55a00dd33863e4cee1dca9661455a21625b2f322
signor1 <emmanuelomemgboji@gmail.com>
2024-02-29 17:03:06 +0100

    feat: added meta token contract

--- contracts/MetaToken.sol
+++ contracts/MetaToken.sol
@@ -3,3 +3,13 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MetaToken is ERC20, Ownable {
+    constructor() ERC20("Meta Token", "MT") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * (10 ** 18));
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
